10a42300e1f49081f139d84101873f91b40ce3e4
Emre-Bilge <emre_bilge_@hotmail.com>
2024-11-05 16:25:09 +0300

    s12task1 add

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,25 @@
+
+export default function Footer() {
+    return (
+        <div className="flex justify-between">
+            <div>
+                <h2>Let’s work together on your next product.</h2>
+
+                <article>
+
+                    <a href="mailto:emre_bilge_@hotmail.com">emre_bilge_@hotmail.com</a>
+                </article>
+            </div>
+            <div>
+                <nav>
+                    <a href="">Personal Blog</a> <span>-</span>
+                    <a href="">Github</a> <span>-</span>
+                    <a href="">Linkedin</a>
+                </nav>
+
+            </div>
+        </div>
+
+
+    )
+}
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,31 @@
+
+export default function Profile() {
+    return (
+
+        <div>
+            <div>
+                <h1>Profile</h1>
+            </div>
+            <div>
+                <h2>Profile</h2>
+                <ul>
+                    <li>Ad Soyad: Emre BİLGE</li>
+                    <li>İkamet Şehri: Kütahya</li>
+                    <li>Egitim Durumu: Lisans</li>
+                    <li>Tercih Ettigi Rol : Developer</li>
+                </ul>
+                <h2>About</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut,
+                    odit laborum aliquam voluptatum nisi mollitia.
+                </p>
+
+                <p>
+                    Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod
+                    deserunt quam temporibus cumque magnam!
+                </p>
+            </div>
+
+        </div>
+    )
+}
